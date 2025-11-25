beb68bc5b23075473cd7a80cb55203090c48aadd
excel <elim0079@student.monash.edu>
2025-11-25 17:23:45 +0800

    setup server folder and client folder

---
+++ client/src/scenes/dashboard/Row3.tsx
@@ -0,0 +1,33 @@
+import DashboardBox from '@/components/DashboardBox'
+import React from 'react'
+
+type Props = {}
+
+const Row3 = (props: Props) => {
+  return (
+    <>
+        <DashboardBox
+            gridArea="g"
+        >
+        </DashboardBox>
+
+        <DashboardBox
+            gridArea="h"
+        >
+        </DashboardBox>
+
+        <DashboardBox
+            gridArea="i"
+        >
+        </DashboardBox>
+
+        <DashboardBox
+            gridArea="j"
+            bgcolor="#fff"
+        >
+        </DashboardBox>
+    </>
+  )
+}
+
+export default Row3
