a29ac5f02429eedd8a31fe34c9317af5edae2193
excel <excelyynxl@gmail.com>
2025-12-05 16:46:26 +0800

    inserting transaction

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
 import KPI from "./models/KPI.js";
 import Product from "./models/Product.js";
-import { kpis, products } from "./data/data.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 // configuration
 dotenv.config();
@@ -25,6 +27,7 @@ app.use(cors());
 // routes setup
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 // mongoose setup 
 const PORT = process.env.PORT;
@@ -33,10 +36,11 @@ mongoose
     .then(async () => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-        // // add data one time only as needed
+        // add data one time only as needed
         await mongoose.connection.db.dropDatabase();
         KPI.insertMany(kpis);
         Product.insertMany(products);
+        Transaction.insertMany(transactions);
     })
     .catch((error) => console.log(`${error} did not connect`));
 
